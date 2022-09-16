215283250486a95ff714c36a13ade56bbcb1b2aa
Archana bhardwaj <ArchanaKumari202068@gmail.com>
2022-09-16 19:18:15 +0530

    Python-Chat-App

---
+++ Host.py
@@ -0,0 +1,23 @@
+from email import message
+import socket
+import sys
+import time
+
+s= socket.socket()
+
+host = socket.gethostname()
+print("Server will start on host:",host)
+port=1234
+s.bind((host,port))
+print("Server is bound successfully")
+s.listen(1)
+conn,addr=s.accept()
+print(addr,"has Connected")
+
+while 1:
+    message=input(str("You>>")) #format should be encoded
+    message=message.encode()
+    conn.send(message)
+    incoming_message=conn.recv(1024)
+    incoming_message=incoming_message.decode()
+    print("client:>>",incoming_message)
